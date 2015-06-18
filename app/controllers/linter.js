c67dd5ce56d083d7579b52793663fa41beeb6781
Jesse Pinho <jesse@jessepinho.com>
2015-06-18 09:12:12 -0500

    Add randomized placeholder values

--- app/controllers/linter.js
+++ app/controllers/linter.js
@@ -29,5 +29,64 @@ export default Ember.Controller.extend({
       return 'Please enter a valid JSON object and pointer.';
     }
     return JSON.stringify(this.get('pointerObject'), null, '  ');
-  })
+  }),
+
+  placeholders: [
+    {
+      pointer: '/westeros/rulers/0',
+      object: {
+        westeros: {
+          kingdomCount: 7,
+          rulers: [
+            'Robert Baratheon',
+            'Joffrey Lannister',
+            'Tommen Lannister'
+          ]
+        }
+      }
+    },
+    {
+      pointer: '/senses/5',
+      object: {
+        senses: [
+          'sight',
+          'sound',
+          'smell',
+          'touch',
+          'taste',
+          'sixth'
+        ]
+      }
+    },
+    {
+      pointer: '/rules/1',
+      object: {
+        members: ['Cornelius', 'Tyler', 'Bob'],
+        rules: [
+          'You do not talk about Fight Club.',
+          'You do not talk about Fight Club.',
+          'Someone yells stop, goes limp, taps out, the fight is over.',
+          'Only two guys to a fight.',
+          'One fight at a time.',
+          'No shirts, no shoes.',
+          'Fights will go on as long as they have to.',
+          'If this is your first night at Fight Club, you have to fight.'
+        ]
+      }
+    }
+  ],
+
+  chosenPlaceholder: Ember.computed('placeholders', function() {
+    var placeholders = this.get('placeholders');
+    var randomIndex = Math.floor(Math.random() * placeholders.length);
+    return placeholders[randomIndex];
+  }),
+
+  objectPlaceholder: Ember.computed('chosenPlaceholder', function() {
+    return JSON.stringify(this.get('chosenPlaceholder').object, null, '  ');
+  }),
+
+  pointerPlaceholder: Ember.computed('chosenPlaceholder', function() {
+    return this.get('chosenPlaceholder').pointer;
+  }),
 });
