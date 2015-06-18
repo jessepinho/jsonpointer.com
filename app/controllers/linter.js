d0eddccc4ba2b67024b59b0abedb9656833836ec
Jesse Pinho <jesse@jessepinho.com>
2015-06-17 21:11:59 -0500

    Tweak formatting

--- app/controllers/linter.js
+++ app/controllers/linter.js
@@ -4,28 +4,26 @@ import pointer from 'npm:json-pointer';
 export default Ember.Controller.extend({
   jsonObject: '',
   jsonPointer: '',
+
   jsonPointerValue: Ember.computed('jsonPointer', 'parsedJsonObject', function() {
     try {
       return pointer.get(this.get('parsedJsonObject'), this.get('jsonPointer'));
     }
-    catch (e) {
-      return undefined;
-    }
+    catch (e) { return undefined; }
   }),
+
   parsedJsonObject: Ember.computed('jsonObject', function() {
-    try {
-      return JSON.parse(this.get('jsonObject'));
-    }
-    catch (e) {
-      return undefined;
-    }
+    try { return JSON.parse(this.get('jsonObject')); }
+    catch (e) { return undefined; }
   }),
+
   formattedJsonObject: Ember.computed('parsedJsonObject', function() {
     if (typeof this.get('parsedJsonObject') === 'undefined') {
       return 'Please enter a valid JSON object.';
     }
     return JSON.stringify(this.get('parsedJsonObject'), null, '  ');
   }),
+
   formattedJsonPointerValue: Ember.computed('jsonPointerValue', function() {
     if (typeof this.get('jsonPointerValue') === 'undefined') {
       return 'Please enter a valid JSON object and pointer.';
