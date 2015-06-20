6bb36e509b03aa9321c69fe51358a8279725f082
Jesse Pinho <jesse@jessepinho.com>
2015-06-20 13:49:31 -0500

    Escape HTML before highlighting; fix failing specs

--- app/helpers/json-stringifier-highlighter.js
+++ app/helpers/json-stringifier-highlighter.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export function jsonStringifierHighlighter(params) {
   var jsonString = params[0];
-  return Ember.String.htmlSafe(jsonString.replace(/(\_JSONPointerHighlight\_)((.|\n)*)(\_\/JSONPointerHighlight\_)/, '<span class="json-pointer-highlight">$2</span>'));
+  var escaped = Ember.Handlebars.Utils.escapeExpression(jsonString);
+  var highlighted = escaped.replace(/(\_JSONPointerHighlight\_)((.|\n)*)(\_\/JSONPointerHighlight\_)/, '<span class="json-pointer-highlight">$2</span>');
+
+  return Ember.String.htmlSafe(highlighted);
 }
 
 export default Ember.HTMLBars.makeBoundHelper(jsonStringifierHighlighter);
--- tests/unit/helpers/json-stringifier-highlighter-test.js
+++ tests/unit/helpers/json-stringifier-highlighter-test.js
@@ -1,21 +1,22 @@
 import { jsonStringifierHighlighter } from '../../../helpers/json-stringifier-highlighter';
 import { highlightStart, highlightEnd } from '../../../utils/json-stringifier';
 import { module, test } from 'qunit';
+import Ember from 'ember';
 
 module('Unit | Helper | json stringifier highlighter');
 
 test('it replaces highlight start/end markers with <span> tags', function(assert) {
   var highlighted = highlightStart + 'bar' + highlightEnd;
-  var result = jsonStringifierHighlighter('foo\n' + highlighted + '\nbaz');
-  var expected = 'foo\n<span class="json-pointer-highlight">bar</span>\nbaz';
+  var result = jsonStringifierHighlighter(['foo\n' + highlighted + '\nbaz']);
+  var expected = Ember.String.htmlSafe('foo\n<span class="json-pointer-highlight">bar</span>\nbaz');
 
-  assert.equal(result, expected);
+  assert.deepEqual(result, expected);
 });
 
 test('it works across multiline highlights', function(assert) {
   var highlighted = highlightStart + 'bar\nbaz' + highlightEnd;
-  var result = jsonStringifierHighlighter('foo\n' + highlighted + '\nquux');
-  var expected = 'foo\n<span class="json-pointer-highlight">bar\nbaz</span>\nquux';
+  var result = jsonStringifierHighlighter(['foo\n' + highlighted + '\nquux']);
+  var expected = Ember.String.htmlSafe('foo\n<span class="json-pointer-highlight">bar\nbaz</span>\nquux');
 
-  assert.equal(result, expected);
+  assert.deepEqual(result, expected);
 });
