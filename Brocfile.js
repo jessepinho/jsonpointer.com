4bba022c0309278c63c68e56aad379016254da0b
Jesse Pinho <jesse@jessepinho.com>
2015-07-26 14:29:39 -0500

    Install MDL

--- Brocfile.js
+++ Brocfile.js
@@ -3,7 +3,10 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
-  'ember-bootstrap': { importBootstrapTheme: true }
+  'ember-bootstrap': { importBootstrapTheme: true },
+  sassOptions: {
+    includePaths: ['bower_components/material-design-lite/src']
+  }
 });
 
 // Use `app.import` to add additional libraries to the generated
@@ -18,5 +21,6 @@ var app = new EmberApp({
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+app.import('bower_components/material-design-lite/material.js');
 
 module.exports = app.toTree();
